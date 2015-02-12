8ecff73d99a2d23adcdc5c85d479cdfe271934ef
wxu <winnielandau@gmail.com>
2015-02-12 10:35:09 -0800

    JSON being returned, setup for Postman exam

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
 
   def index
-    head :ok
+#    head :ok
+
+    # render json: Message.first #, Message.limit(10), Message.all
+    render json: Message.order(created_at: :desc).limit((params[:limit] || 10)  
+
   end
 
 end
